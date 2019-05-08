fe69d3aeec1de9784ff6d51921b6604df86f97f9
Saeed Ganji <shahabganji@gmail.com>
2019-05-08 23:38:46 +0430

    chores( Some ) method
    Change the return type of Some<T> method to be Option<T>,
    hence Some<T> is a Return function for Option<T>
    
    Changes committed:
    	modified:   src/Nyx/Extensions/OptionExtensions.cs
    	modified:   src/Nyx/Helpers/OptionHelpers.cs
    	modified:   src/Nyx/Option.cs

--- src/Nyx/Helpers/OptionHelpers.cs
+++ src/Nyx/Helpers/OptionHelpers.cs
@@ -5,6 +5,6 @@ namespace Nyx.Helpers
     public static class OptionHelpers
     {
         public static None None => None.Default;
-        public static Some<T> Some<T>(T value) => new Some<T>(value);
+        public static Option<T> Some<T>(T value) => new Some<T>(value);
     }
 }
--- src/Nyx/Option.cs
+++ src/Nyx/Option.cs
@@ -5,49 +5,40 @@ using Nyx.Helpers;
 using Nyx.Option;
 using static Nyx.Helpers.OptionHelpers;
 
-namespace Nyx
-{
-    public struct Option<T>
-    {
+namespace Nyx {
+    public struct Option<T> {
         private readonly bool _isSome;
         private readonly T _value;
 
-        private Option(T value)
-        {
+        private Option(T value) {
             _value = value;
             _isSome = true;
         }
 
-        public static implicit operator Option<T>(None _)
-        {
+        public static implicit operator Option<T>(None _) {
             return new Option<T>();
         }
 
-        public static implicit operator Option<T>(Some<T> some)
-        {
+        public static implicit operator Option<T>(Some<T> some) {
             return new Option<T>(some.Value);
         }
 
-        public static implicit operator Option<T>(T value)
-        {
+        public static implicit operator Option<T>(T value) {
             return value == null ? (Option<T>) OptionHelpers.None : Some(value);
         }
 
-        public TR Match<TR>(Func<TR> none, Func<T, TR> some)
-        {
+        public TR Match<TR>(Func<TR> none, Func<T, TR> some) {
             return _isSome ? some(_value) : none();
         }
 
-        public Task<TR> MatchAsync<TR>(Func<Task<TR>> noneAsync, Func<T, Task<TR>> someAsync)
-        {
+        public Task<TR> MatchAsync<TR>(Func<Task<TR>> noneAsync, Func<T, Task<TR>> someAsync) {
             return _isSome ? someAsync(_value) : noneAsync();
         }
-        
-        public IEnumerable<T> AsEnumerable()
-        {
+
+        public IEnumerable<T> AsEnumerable() {
             if (this._isSome)
                 yield return this._value;
         }
-        
+
     }
 }
