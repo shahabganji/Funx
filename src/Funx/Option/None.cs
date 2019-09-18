501c2b900c737e0909eb31c4d35a61e1839130e9
Saeed Ganji <shahabganji@gmail.com>
2019-09-18 12:41:02 +0430

    test(Option): Add unit tests for MatchAsync in Option, and other missing methods

--- src/Funx/Option/None.cs
+++ src/Funx/Option/None.cs
@@ -3,5 +3,10 @@ namespace Funx.Option
     public struct None
     {
         internal static readonly None Default = new None();
+
+        public override string ToString()
+        {
+            return "None";
+        }
     }
 }
--- tests/Funx/OptionTests.cs
+++ tests/Funx/OptionTests.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading;
 using System.Threading.Tasks;
+using Funx.Option;
 using Xunit;
 using static Funx.Helpers;
 
@@ -12,7 +15,7 @@ namespace Funx.Tests
         [Fact]
         public void NoneShouldBeAnOptionOfT()
         {
-            Option<bool> option = None;
+            Option<bool> option = Helpers.None;
 
             Assert.IsType<Option<bool>>(option);
         }
@@ -28,7 +31,7 @@ namespace Funx.Tests
         [Fact]
         public async Task WhenNoneMatchAsyncMethodShouldCallNoneFuncAsync()
         {
-            Option<string> noneOpt = None;
+            Option<string> noneOpt = Helpers.None;
 
             Task<bool> NoneFuncAsync()
             {
@@ -48,7 +51,7 @@ namespace Funx.Tests
         [Fact]
         public void WhenNoneMatchMethodShouldCallNoneFunc()
         {
-            Option<string> noneOpt = None;
+            Option<string> noneOpt = Helpers.None;
 
             var noneCalled = noneOpt.Match(() => true, v => false);
 
@@ -58,7 +61,7 @@ namespace Funx.Tests
         [Fact]
         public async Task WhenSomeMatchAsyncMethodShouldNotCallNoneFuncAsync()
         {
-            Option<string> someOpt = Some("fake");
+            var someOpt = Some("fake");
 
             Task<bool> NoneFuncAsync()
             {
@@ -73,6 +76,70 @@ namespace Funx.Tests
             var noneCalled = await someOpt.MatchAsync(NoneFuncAsync, SomeFuncAsync);
 
             Assert.True(noneCalled);
+        }
+
+        [Fact]
+        public async Task WhenIsSomeThenCallSomeAsyncAndNotNone()
+        {
+            var option = Some(11);
+
+            bool NoneFunc() => false;
+            Task<bool> SomeAsync(int value) => Task.FromResult(value == 11);
+
+            var xxx = SynchronizationContext.Current;
+
+            var hasCalledSome = await option.MatchAsync(NoneFunc, SomeAsync)
+                .ConfigureAwait(false);
+
+            Assert.True(hasCalledSome);
+
+        }
+
+        [Fact]
+        public async Task WhenIsNoneThenCallNoneAndNotSomeAsync()
+        {
+            var option = (Option<int>)Helpers.None;
+
+            bool NoneFunc() => true;
+            Task<bool> SomeAsync(int value) => Task.FromResult(false);
+
+            var hasCalledNone = await option.MatchAsync(NoneFunc, SomeAsync)
+                .ConfigureAwait(false);
+
+            Assert.True(hasCalledNone);
+
+        }
+
+        [Fact]
+        public async Task WhenIsSomeThenCallSomeAndNotNoneAsync()
+        {
+            var option = Some(11);
+
+            Task<bool> NoneFuncAsync() => Task.FromResult(false);
+            bool SomeFunc(int value) => value == 11;
+
+            var xxx = SynchronizationContext.Current;
+
+            var hasCalledSome = await option.MatchAsync(NoneFuncAsync, SomeFunc)
+                .ConfigureAwait(false);
+
+            Assert.True(hasCalledSome);
+
+        }
+
+        [Fact]
+        public async Task WhenIsNoneThenCallNoneAsyncAndNotSome()
+        {
+            var option = (Option<int>)Helpers.None;
+
+            Task<bool> NoneFuncAsync() => Task.FromResult(true);
+            bool SomeFunc(int value) => false;
+
+            var hasCalledNone = await option.MatchAsync(NoneFuncAsync, SomeFunc)
+                .ConfigureAwait(false);
+
+            Assert.True(hasCalledNone);
+
         }
 
         [Fact]
@@ -106,10 +173,31 @@ namespace Funx.Tests
 
 
         [Fact]
+        public void ShouldNullObjectBeNotEqualToSomeButEqualToNone()
+        {
+            object obj1 = Some(1);
+            object obj2 = Some(2);
+
+            object objNone = (Option<int>)Helpers.None;
+            object objNoneStr = (Option<string>)Helpers.None;
+
+            var some = Some(1);
+            Option<int> none = Helpers.None;
+
+            Assert.True(some.Equals(obj1));
+            Assert.False(some.Equals(obj2));
+            Assert.False(some.Equals(objNone));
+
+            Assert.False(none.Equals(obj1));
+            Assert.True(none.Equals(objNone));
+            Assert.False(none.Equals(objNoneStr));
+        }
+
+        [Fact]
         public void ShouldTwoNoneValuesOrNullBeEqual()
         {
-            var first = (Option<int>) None;
-            var other = (Option<int>) None;
+            var first = (Option<int>) Helpers.None;
+            var other = (Option<int>) Helpers.None;
 
             var actual = first.Equals(other);
 
@@ -119,7 +207,7 @@ namespace Funx.Tests
         [Fact]
         public void ShouldNoneAndSomeNotBeEqual()
         {
-            var first = (Option<int>) None;
+            var first = (Option<int>) Helpers.None;
             var other = Some(1);
 
             var actual = first.Equals(other);
@@ -128,31 +216,113 @@ namespace Funx.Tests
         }
 
         [Fact]
-        public void ShouldEqualityOperatorReturnTrueOnOptionTypes()
+        public void ShouldEqualityOperatorReturnCorrectOnOptionTypes()
         {
-            var firstNone = (Option<int>) None;
-            var otherNone = (Option<int>) None;
-
-            var actualNone = firstNone == otherNone;
+            var firstNone = (Option<int>) Helpers.None;
+            var otherNone = (Option<int>) Helpers.None;
 
             var firstSome = Some(1);
             var otherSome = Some(1);
 
-            var actualSome = firstSome == otherSome;
+            var secondSome = Some(2);
 
-            Assert.True(actualNone);
-            Assert.True(actualSome);
+
+            Assert.True(firstSome == otherSome);
+            Assert.True(firstNone == otherNone);
+
+            Assert.False(firstNone == firstSome);
+            Assert.False(otherNone == otherSome);
+
+            Assert.False(secondSome == firstSome);
+            Assert.False(secondSome == firstNone);
+        }
+
+        [Fact]
+        public void ShouldInEqualityOperatorReturnCorrectOnOptionTypes()
+        {
+            var firstNone = (Option<int>) Helpers.None;
+            var otherNone = (Option<int>) Helpers.None;
+
+            var firstSome = Some(1);
+            var otherSome = Some(1);
+
+            var secondSome = Some(2);
+
+
+            Assert.False(firstSome != otherSome);
+            Assert.False(firstNone != otherNone);
+
+            Assert.True(firstNone != firstSome);
+            Assert.True(otherNone != otherSome);
+
+            Assert.True(secondSome != firstSome);
+            Assert.True(secondSome != firstNone);
         }
 
         [Fact]
         public void ShouldEqualityOperatorReturnTrueOnOptionAndPrimitiveTypes()
         {
-            var firstSome = Some(1);
-            var other = 1;
+            Option<string> none = Helpers.None;
+            var firstSome = Some("1");
+            string str = null; // equals to None
 
-            var actualSome = firstSome == other;
+            
+            Assert.True( firstSome == "1");
+            Assert.True(none == str);
 
-            Assert.True(actualSome);
+            Assert.False(firstSome == "2");
+            Assert.False(firstSome == str);
+
+
+            Assert.True( "1"== firstSome );
+            Assert.True(str== none );
+
+            Assert.False("2"== firstSome );
+            Assert.False(str== firstSome );
+
         }
+
+        [Fact]
+        public void ShouldInEqualityOperatorReturnTrueOnOptionAndPrimitiveTypes()
+        {
+            Option<string> none = Helpers.None;
+            var firstSome = Some("1");
+            string str = null; // equals to None
+
+            
+            Assert.False( firstSome != "1");
+            Assert.False(none != str);
+
+            Assert.True(firstSome != "2");
+            Assert.True(firstSome != str);
+
+
+            Assert.False( "1"!= firstSome );
+            Assert.False(str!= none );
+
+            Assert.True("2"!= firstSome );
+            Assert.True(str!= firstSome );
+
+        }
+
+        [Fact]
+        public void ToString_ShouldPrintNoneWhenNone()
+        {
+            var none = (Option<int>)Helpers.None;
+            var nonePure = Helpers.None;
+
+            Assert.Equal(none.ToString() , $"None");
+            Assert.Equal(nonePure.ToString() , $"None");
+
+        }
+
+        [Fact]
+        public void ToString_ShouldPrintSomeWhenHasValues()
+        {
+            var none = Some(1);
+            
+            Assert.Equal(none.ToString() , $"Some(1)");
+        }
+
     }
 }
