3bf035edfd437ae41e055ef9741fe48106ec889f
Saeed Ganji <shahabganji@gmail.com>
2019-10-06 22:16:02 +0330

    add: console app for test

---
+++ tests/Funx.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+
+using Funx.Extensions;
+using static Funx.Helpers;
+
+
+namespace Funx.Console {
+    
+    internal static class Program 
+    {
+        private static void Main(string[] args) {
+
+            var str = Some("value");
+
+            Option<string> IsValid(string v)
+            {
+                if (v.StartsWith("v"))
+                    return v;
+
+                return None;
+            }
+
+            var upperCase = str.Bind(IsValid);
+
+            upperCase.ForEach(WriteLine);
+
+
+        }
+    }
+}
