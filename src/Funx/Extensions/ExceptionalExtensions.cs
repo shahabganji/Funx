f22a5634a4cc92b8d40f24789c51bd9276e83461
Saeed Ganji <shahabganji@gmail.com>
2020-03-15 11:50:32 +0330

    test(exceptional): add unit tests for exceptional

--- src/Funx/Extensions/ExceptionalExtensions.cs
+++ src/Funx/Extensions/ExceptionalExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using static Funx.Helpers;
 using Unit = System.ValueTuple;
 
 namespace Funx.Extensions
@@ -11,11 +12,18 @@ namespace Funx.Extensions
         public static Exceptional<R> Select<T,R>(this Exceptional<T> exp, Func<T, R> f)
             => exp.Map(f);
 
-        public static Exceptional<Unit> ForEach<T>(this Exceptional<T> exp, Action<T> act)
+        public static void ForEach<T>(this Exceptional<T> exp, Action<T> act)
             => exp.Map(act.ToFunc());
 
         public static Exceptional<R> Bind<T, R>(this Exceptional<T> exp, Func<T, Exceptional<R>> f)
-            => exp.Match(ex => ex, f);
+            => exp.Match(
+                exception => exception,
+                f);
+        
+        public static Option<R> Bind<T, R>(this Exceptional<T> exp, Func<T, Option<R>> f)
+            => exp.Match(
+                exception => None,
+                f);
 
         public static  Result<T> ToResult<T>(this Exceptional<T> exceptional)
             => exceptional.Match<Result<T>>(exp => new Error(exp.Message), data => data);
